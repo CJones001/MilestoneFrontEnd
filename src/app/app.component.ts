df927a76ba699e22da0373fd74d671472d5517c9
CJones001 <147557670+CJones001@users.noreply.github.com>
2025-03-25 17:25:41 -0700

    Service made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'MilestoneFrontEnd';
+  displayRestaurant() {
+    this.router.navigate(['/display-restaurant']);  }
+
+  constructor(private router: Router, private http: HttpClient) {}
+
 }
