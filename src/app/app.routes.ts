cc444a4648685b577bcb765e8e151bef19bcec36
CJones001 <147557670+CJones001@users.noreply.github.com>
2025-04-22 23:17:50 -0700

    Added some CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { CreateRestaurantComponent } from '../create-restaurant/create-restaurant.component';
-import { CreateFoodComponent } from '../create-food/create-food.component';
 import { ListFoodsComponent } from '../list-foods/list-foods.component';
 import { ListRestaurantsComponent } from '../list-restaurants/list-restaurants.component';
 import { DisplayRestaurantComponent } from '../display-restaurant/display-restaurant.component';
@@ -9,7 +8,6 @@ import { DeleteRestaurantComponent } from '../delete-restaurant/delete-restauran
 
 export const routes: Routes = [
   {path: 'create', component: CreateRestaurantComponent},
-  {path: 'create', component: CreateFoodComponent},
   {path: 'list-foods', component: ListFoodsComponent},
   {path: 'list-restaurants', component: ListRestaurantsComponent},
   {path: 'display-restaurant/:id', component: DisplayRestaurantComponent},
